419365df30e6aeea0f104c86a9e678241c0c587b
Azelliott <jukic.alen@gmail.com>
2022-11-26 08:47:41 +0000

    Wire up home app and basic template

--- GreenSeed/settings.py
+++ GreenSeed/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +73,10 @@ ROOT_URLCONF = 'GreenSeed.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
