c4abdbba73d48518bd69a77910a031a684543dd2
Azelliott <jukic.alen@gmail.com>
2022-11-28 06:24:46 +0000

    Update allowed hosts in settings.py

--- GreenSeed/settings.py
+++ GreenSeed/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greenseed-azelliott.herokuapp.com', '127.0.0.1' ]
 
 
 # Application definition
