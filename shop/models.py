9551eb4fdefa8208faeac99ac0772a40bcd7e8c9
Azelliott <jukic.alen@gmail.com>
2022-12-28 07:27:00 +0000

    Create Products model, add folder to media

--- shop/models.py
+++ shop/models.py
@@ -17,3 +17,35 @@ class Category(models.Model):
 
     def __str__(self):
         return self.description
+
+
+# Product model
+class Product(models.Model):
+    '''Product model'''
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products', null=True, blank=True)
+    availability = models.BooleanField(default=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True,
+                                 blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+    def __str__(self):
+        return self.description
+
+    def __str__(self):
+        return self.price
+
+    def __str__(self):
+        return self.availability
+
+    def __str__(self):
+        return self.rating
