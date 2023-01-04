c1a876606b6980444ed6e42de0f66c0d68b18acc
Azelliott <jukic.alen@gmail.com>
2023-01-04 12:48:24 +0000

    Create models for checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.conf import settings
+import uuid
+from django.db.models import Sum
+from shop.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    '''Order model'''
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date_placed = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0, editable=False)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                        null=False, default=0)
+
+    def _generate_order_number(self):
+        '''Generate a random, unique order number using UUID'''
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        '''Update grand total each time a line item is added,
+        accounting for delivery costs'''
+        self.order_total = self.items.aggregate(Sum('item_total'))['item_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        '''Override the original save method to set the order number
+        if it hasn't been set already'''
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderItem(models.Model):
+    '''Order item model'''
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='items')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    item_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                     null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        '''Override the original save method to set the item total
+        and update the order total'''
+        self.item_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Product: {self.product.name} on order {self.order.order_number}'
