6824240aeb1d4868760ad3622969bb5175810e97
Azelliott <jukic.alen@gmail.com>
2023-01-04 13:14:22 +0000

    Add checkout form, admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+# OrderItem admin inline
+class OrderItemInline(admin.TabularInline):
+    '''Inline admin for OrderItem model'''''
+    model = OrderItem
+    readonly_fields = ('item_total',)
+
+
+# Order admin
+class OrderAdmin(admin.ModelAdmin):
+    '''Admin for Order model'''
+    inlines = (OrderItemInline,)
+
+    readonly_fields = ('order_number', 'date_placed', 'delivery_cost',
+                       'order_total', 'grand_total',)
+
+    fields = ('order_number', 'date_placed', 'full_name', 'email',
+              'phone_number', 'country', 'postcode', 'city',
+              'street_address1', 'street_address2', 'county',
+              'delivery_cost', 'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date_placed', 'full_name', 'order_total',
+                    'delivery_cost', 'grand_total',)
+
+    ordering = ('-date_placed',)
+
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderItem
+
+
+@receiver(post_save, sender=OrderItem)
+def update_on_save(sender, instance, created, **kwargs):
+    '''Update order total on lineitem update/create'''
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderItem)
+def update_on_delete(sender, instance, **kwargs):
+    '''Update order total on lineitem delete'''
+    instance.order.update_total()
