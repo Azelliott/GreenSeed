dcc3ba63247b87de4fdf99fcd4980810ccda37d5
Azelliott <jukic.alen@gmail.com>
2023-01-05 10:36:26 +0000

    Add order details to checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -36,21 +36,12 @@ def checkout(request):
                 try:
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
-                        order_line_item = OrderItem(
+                        order_item = OrderItem(
                             order=order,
                             product=product,
                             quantity=item_data,
                         )
-                        order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                product_size=size,
-                            )
-                            order_line_item.save()
+                        order_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your cart wasn't found in our database. "
