2d62fc7c44cd253f93252f2a5b0d8392dc4e198f
Azelliott <jukic.alen@gmail.com>
2023-01-05 23:08:26 +0000

    Updated webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderItem
 from shop.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -43,6 +44,23 @@ class WebhookHandler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
+            
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -77,6 +95,7 @@ class WebhookHandler:
                 for item.id, item_data in json.loads(cart).items():
                     order = Order.objects.create(
                         full_name=shipping_details.name,
+                        user_profile=profile,
                         email=billing_details.email,
                         phone_number=shipping_details.phone,
                         country=shipping_details.address.country,
