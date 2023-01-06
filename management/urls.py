cd704134d47c54bafc0383552a1812d3b440b169
Azelliott <jukic.alen@gmail.com>
2023-01-06 03:23:12 +0000

    Finish edit product functionality, add view and template to management app

--- management/urls.py
+++ management/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<product_id>/', views.edit_product, name='edit_product'),
 ]
